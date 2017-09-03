cac8999b5992b2666a67d2cfd023b5ac3d5899fe
robstallion <robertefrancis18@gmail.com>
2017-09-03 13:45:01 +0100

    go back to rendering html with link to dl certfile as shown in nelsons readme https://github.com/dwyl/learn-heroku/blob/master/SSL-certificate-step-by-step-setup-instructions.md

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -6,13 +6,6 @@ defmodule AzureDeploymentTest.PageController do
   end
 
   def file(conn, _params) do
-    file =
-      Application.app_dir(:azure_deployment_test, "priv")
-      |> Path.join("/static/images/certfile")
-      |> File.read!("web/static/assets/images/certfile")
-    conn
-    |> send_resp(200, file)
-    # render conn, "index.html"
-    # render conn, test: test
+    render conn, "file.html"
   end
 end
