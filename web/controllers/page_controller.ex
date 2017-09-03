1e09b2ade1ab4fd110d7949e315795529e511df6
robstallion <robertefrancis18@gmail.com>
2017-09-03 20:07:49 +0100

    andrews approach

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -4,11 +4,4 @@ defmodule AzureDeploymentTest.PageController do
   def index(conn, _params) do
     render conn, "index.html"
   end
-
-  def file(conn, _params) do
-    conn
-    # |> put_resp_content_type("application/octet-stream", "utf-8")
-    # |> send_file(200, "web/static/assets/images/LVE11x1004ntLGhaqTexNwyeFd002MNeCdPe8lX9MQs")
-    |> render("file.html")
-  end
 end
