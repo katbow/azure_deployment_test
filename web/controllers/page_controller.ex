5567eb7baa8880323833b5a38fdeed5948b0ea5b
robstallion <robertefrancis18@gmail.com>
2017-09-03 19:57:11 +0100

    update with new cert data

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -8,7 +8,7 @@ defmodule AzureDeploymentTest.PageController do
   def file(conn, _params) do
     conn
     # |> put_resp_content_type("application/octet-stream", "utf-8")
-    # |> send_file(200, "web/static/assets/images/CvUFcIns2Yn5mhxfxigLGNJAAsWL2DaPwuSLSC2iUYw")
+    # |> send_file(200, "web/static/assets/images/LVE11x1004ntLGhaqTexNwyeFd002MNeCdPe8lX9MQs")
     |> render("file.html")
   end
 end
--- web/router.ex
+++ web/router.ex
@@ -17,9 +17,9 @@ defmodule AzureDeploymentTest.Router do
     pipe_through :browser # Use the default browser stack
 
     get "/", PageController, :index
-    get "/.well-known/acme-challenge/CvUFcIns2Yn5mhxfxigLGNJAAsWL2DaPwuSLSC2iUYw", PageController, :file
+    get "/.well-known/acme-challenge/LVE11x1004ntLGhaqTexNwyeFd002MNeCdPe8lX9MQs", PageController, :file
   end
-  
+
   # Other scopes may use custom stacks.
   # scope "/api", AzureDeploymentTest do
   #   pipe_through :api
