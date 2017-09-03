792cfebb9397453349364ccef38086a900027392
robstallion <robertefrancis18@gmail.com>
2017-09-03 13:28:45 +0100

    update controller to read correct file

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -6,10 +6,13 @@ defmodule AzureDeploymentTest.PageController do
   end
 
   def file(conn, _params) do
-    file = File.read!("web/static/assets/images/certfile")
+    file =
+      Application.app_dir(:azure_deployment_test, "priv")
+      |> Path.join("/static/images/certfile")
+      |> File.read!("web/static/assets/images/certfile")
     conn
+    |> send_resp(200, file)
     # render conn, "index.html"
     # render conn, test: test
-    |> send_resp(200, file)
   end
 end
