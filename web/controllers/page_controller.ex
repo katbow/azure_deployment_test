779fd34c44df27b5f28e59ba5eecc9bd1185b36d
robstallion <robertefrancis18@gmail.com>
2017-09-03 14:57:49 +0100

    make it so that the file is downloaded at this endpoint. trying to replicate what is shown in readme here ... https://github.com/dwyl/learn-heroku/blob/master/SSL-certificate-step-by-step-setup-instructions.md

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -6,6 +6,8 @@ defmodule AzureDeploymentTest.PageController do
   end
 
   def file(conn, _params) do
-    render conn, "file.html"
+    conn
+    |> put_resp_content_type("application/octet-stream", "utf-8")
+    |> send_file(200, "web/static/assets/images/Pg2uKspKLyTq4LDi8WiXwlnRjYkMSc0fGW5-oRG_r54")
   end
 end
