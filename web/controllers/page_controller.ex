919a0ef7f1d2f5d655b1ec649558e0d9be50728e
robstallion <robertefrancis18@gmail.com>
2017-09-03 19:39:46 +0100

    stop serving static file and render file html

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -7,7 +7,8 @@ defmodule AzureDeploymentTest.PageController do
 
   def file(conn, _params) do
     conn
-    |> put_resp_content_type("application/octet-stream", "utf-8")
-    |> send_file(200, "web/static/assets/images/F7mVdvnEuajgfVIFRzVcuK5-vyURg3GKi6rQZ_opp8k")
+    # |> put_resp_content_type("application/octet-stream", "utf-8")
+    # |> send_file(200, "web/static/assets/images/F7mVdvnEuajgfVIFRzVcuK5-vyURg3GKi6rQZ_opp8k")
+    |> render("file.html")
   end
 end
